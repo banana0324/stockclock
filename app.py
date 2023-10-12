6374274482fbd0f16e98f7047fbce4a0da2b130b
HAO <david0324t@gmail.com>
2023-10-12 10:18:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,152 +1,152 @@
-# from flask import Flask, request, abort
-
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
-# from linebot.exceptions import (
-#     InvalidSignatureError
-# )
-# from linebot.models import *
-
-# #======python的函數庫==========
-# import tempfile, os
-# import datetime
-# import openai
-# import time
-# #======python的函數庫==========
-
-# app = Flask(__name__)
-# static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# # Channel Access Token
-# line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# # Channel Secret
-# handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# # OPENAI API Key初始化設定
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-
-
-# def GPT_response(text):
-#     # 接收回應
-#     response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-#     print(response)
-#     # 重組回應
-#     answer = response['choices'][0]['text'].replace('。','')
-#     return answer
-
-
-# # 監聽所有來自 /callback 的 Post Request
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-#     return 'OK'
-
-
-# # 處理訊息
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     msg = event.message.text
-#     try:
-#         GPT_answer = GPT_response(msg)
-#         print(GPT_answer)
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
-#     except:
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
-        
-
-# @handler.add(PostbackEvent)
-# def handle_message(event):
-#     print(event.postback.data)
-
-
-# @handler.add(MemberJoinedEvent)
-# def welcome(event):
-#     uid = event.joined.members[0].user_id
-#     gid = event.source.group_id
-#     profile = line_bot_api.get_group_member_profile(gid, uid)
-#     name = profile.display_name
-#     message = TextSendMessage(text=f'{name}歡迎加入')
-#     line_bot_api.reply_message(event.reply_token, message)
-        
-        
-# import os
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
-
-import os
-import sys
-from argparse import ArgumentParser
-
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import openai
+import time
+#======python的函數庫==========
 
 app = Flask(__name__)
-
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+# OPENAI API Key初始化設定
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
+def GPT_response(text):
+    # 接收回應
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    print(response)
+    # 重組回應
+    answer = response['choices'][0]['text'].replace('。','')
+    return answer
+
+
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    user_id = event.source.user_id
-    print('user_id:' + user_id)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="luka: " + event.message.text)
-    )
+def handle_message(event):
+    msg = event.message.text
+    try:
+        GPT_answer = GPT_response(msg)
+        print(GPT_answer)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
+        
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
 
 
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+        
+        
+import os
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
-    app.run(debug=options.debug, port=options.port)
+# import os
+# import sys
+# from argparse import ArgumentParser
+
+# from flask import Flask, request, abort
+# from linebot import (
+#     LineBotApi, WebhookHandler
+# )
+# from linebot.exceptions import (
+#     InvalidSignatureError
+# )
+# from linebot.models import (
+#     MessageEvent, TextMessage, TextSendMessage,
+# )
+
+# app = Flask(__name__)
+
+# # get channel_secret and channel_access_token from your environment variable
+# channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+# channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+# if channel_secret is None:
+#     print('Specify LINE_CHANNEL_SECRET as environment variable.')
+#     sys.exit(1)
+# if channel_access_token is None:
+#     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+#     sys.exit(1)
+
+# line_bot_api = LineBotApi(channel_access_token)
+# handler = WebhookHandler(channel_secret)
+
+
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+
+#     return 'OK'
+
+
+# @handler.add(MessageEvent, message=TextMessage)
+# def message_text(event):
+#     user_id = event.source.user_id
+#     print('user_id:' + user_id)
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text="luka: " + event.message.text)
+#     )
+
+
+# if __name__ == "__main__":
+#     arg_parser = ArgumentParser(
+#         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+#     )
+#     arg_parser.add_argument('-p', '--port', default=8000, help='port')
+#     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+#     options = arg_parser.parse_args()
+
+#     app.run(debug=options.debug, port=options.port)
